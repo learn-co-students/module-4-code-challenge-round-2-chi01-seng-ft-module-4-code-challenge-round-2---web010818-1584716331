6e526877e246bada476a4700c90d41123dff5f84
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-03-20 11:41:24 -0500

    max done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,35 @@
-import React from 'react'
+import React from "react";
+import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = props => {
+  const renderTransactions = () => {
+    return props.transactions.map(transaction => {
+      return <Transaction key={transaction.id} transaction={transaction} />;
+    });
+  };
 
   return (
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
+            <h3 className="ui center aligned header">Posted At</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
 
-        {"... your code here..."}
-
+        {renderTransactions()}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
