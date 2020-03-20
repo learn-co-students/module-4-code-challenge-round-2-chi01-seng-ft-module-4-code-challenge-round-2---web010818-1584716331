660046741c5f871d167f0f8f4d6d88e937ece852
github username <github email address>
2020-03-20 10:58:40 -0500

    Sam Zandi Mod 4 Code Challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      storedTransactions: [],
+      transactions: [],
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
-
-  handleChange(event) {
-    // your code here
+  componentDidMount(){
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+    .then(resp => resp.json())
+    .then(transactions => {
+      this.setState({
+        storedTransactions: transactions,
+        transactions: transactions
+      })
+    })
   }
 
+  handleChange = (event) => {
+    event.persist();
+    const filteredTransactions = this.state.storedTransactions.filter(transaction => {
+      if ((transaction.description.toLowerCase().includes(event.target.value.toLowerCase())) || (transaction.category.toLowerCase().includes(event.target.value.toLowerCase()))){
+        return true
+      } else return false
+    });
+    this.setState({
+      transactions: filteredTransactions
+    })
+  }
+
+
+ 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search  handleChange={this.handleChange} />
+        <TransactionsList transactions={this.state.transactions} key={345353}/>
       </div>
     )
   }
