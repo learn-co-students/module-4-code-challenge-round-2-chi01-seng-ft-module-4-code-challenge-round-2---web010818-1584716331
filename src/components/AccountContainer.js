efc0f7cf9f1fd4d6113fdeec57a2181ef4e6bfa0
learn <github email address>
2020-03-20 10:02:45 -0700

    PC issues couldn't workout bugs

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,27 +3,69 @@ import TransactionsList from './TransactionsList'
 import Search from './Search'
 import {transactions} from '../transactionsData'
 
+
+const API = "https://boiling-brook-94902.herokuapp.com/transactions"
+
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state={
+      transactions: []
+  }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      transactions: data
+    }))
   }
 
-  render() {
+  handleChange = (event) => {
+    console.log(event.target.value)
+    const searchArr = this.state.transactions.filter(transaction =>{
+      if(transaction.description.includes(event.target.value) || transaction.category.includes(event.target.vaue)){return transaction}
+      else{return null}
+    })
+      
+    // if else(transaction.category.includes(event.target.vaue)) {return transaction}
+    // else {return null}
+  //   if (condition1)
+  //   statement1
+  // else if (condition2)
+  //   statement2
+  // else if (condition3)
+  //   statement3
+  // ...
+  // else
+  //   statementN
+      
+      // () || )
 
+    // console.log(this.state.transactions)
+
+    this.setState({
+      transactions: searchArr
+    })
+  }
+  // const result = words.filter(word => word.length > 6);
+  // const found = array1.find(element => element > 10);
+
+
+
+
+  render() {
+    // console.log(this.state.transactions)
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange}/>
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     )
   }
