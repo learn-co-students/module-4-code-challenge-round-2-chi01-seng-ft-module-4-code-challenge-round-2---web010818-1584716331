5e472bc46a82ec93f146ed731c19fd68fd228e48
Andrea <andreabarr87@gmail.com>
2020-03-20 12:20:37 -0500

    fin

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,26 +4,60 @@ import Search from './Search'
 import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
   constructor() {
     super()
-
+    this.state = {
+      transactions: transactions,
+      searchValue: ' '
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(resp => resp.json())
+      .then(transactionData => 
+        this.setState({
+          transactions: transactionData,
+          matchingTransactions: []
+        }))
   }
+
+  handleChange = (event) => {
+    this.setState({
+      searchValue: event.target.value
+    })
+    // console.log(this.state.searchValue)
+  }
+
+
+  //take this.state.searchvalue
+  //map through transaction descriptions to see if any of them include this.state.searchvalue
+  //map through transaction categories to see if any of them include this.state.searchvalue
+  //if* so, update state to display only those that match
+  //make sure what is displayed in transactionsList matches "matchingTransactions" and not just "Transactions"
+
+  handleClick = () => {
+    console.log(this.state.transactions)
+    const filtered = this.state.transactions.filter((transaction) => {
+      return transaction.description === this.state.searchValue
+    })
+    this.setState({
+      matchingTransactions: filtered
+    })
+    console.log(filtered)
+  }
+
+
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search transactionsData={this.state.transactions} handleChange={this.handleChange} handleClick={this.handleClick}/>
+        <TransactionsList transactionsData={this.state.transactions} handleChange={this.handleChange} handleClick={this.handleClick}/>
       </div>
     )
   }
