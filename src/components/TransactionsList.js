faa247d9a539fbc3acfd828a4c0c68b0d4bfc8a1
m-gizzi <matthewgizzi@verizon.net>
2020-03-20 10:51:34 -0500

    Matthew

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+
+  const renderTransactions = () => {
+    return props.transactions.map(transaction => {
+      return <Transaction key={transaction.id} transaction={transaction} />
+    })
+  }
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +35,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {renderTransactions()}
 
       </tbody>
     </table>
